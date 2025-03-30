9e4bac267cd2bf5333cdab99ecb70a71254fefd6
RR45-dev <id>
2025-03-30 07:52:59 -0600

    Initial commit after reset

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/yraghuram/",
-  plugins: [react()]
-})
+  base: "/yraghuram/", // Ensure the correct base path for GitHub Pages
+  plugins: [react()],
+  optimizeDeps: {
+    // Explicitly optimize these dependencies if needed
+    include: ['framer-motion', '@emailjs/browser', 'swiper', 'react-modal'],
+    exclude: ['framer-motion', 'swiper'], // Exclude problematic dependencies from Vite optimization
+  },
+  build: {
+    commonjsOptions: {
+      // This ensures mixed ES module types are handled during build
+      transformMixedEsModules: true,
+    },
+  },
+});
